dc20bb24036d21e6788b23a03b006efc36a1c252
iyassalah <iyasgamer01.p@gmail.com>
2022-03-14 10:54:36 +0200

    slider section start

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ function burgerMenu() {
       x.className = "";
     }
 }
+
+
+
+var slide_index = 1 ; 
+ 
+let slides = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "4.jpg"];   
+    displaySlides(slide_index); 
+        
+    function nextSlide(n) {  
+        displaySlides(slide_index += n);  
+    }   
+    function displaySlides(n) {  
+        var i;  
+        if (n > slides.length) {
+            slide_index = 1
+        }  
+        if (n < 1) {
+            slide_index = slides.length
+        }
+
+        $(document).ready(function(){
+            $('#slide').attr('src', slides[slide_index]);
+        }); 
+    }  
